312f177a72ce4974351542b728c5e35ab4f753bb
村松龍之介 <riscait@imac-mr.local>
2017-04-29 20:36:52 +0900

    storyboardでVCを追加

---
+++ SoundsLyric/SongEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SongEditViewController.swift
+//  SoundsLyric
+//
+//  Created by 村松龍之介 on 2017/04/29.
+//  Copyright © 2017年 ryunosuke.muramatsu. All rights reserved.
+//
+
+import UIKit
+
+class SongEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

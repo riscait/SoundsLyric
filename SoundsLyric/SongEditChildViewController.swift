3f6852a736bb31907a2d58034cbf5ba7d7786474
村松龍之介 <riscait@imac-mr.local>
2017-05-15 02:39:04 +0900

    寝る前

--- SoundsLyric/SongEditChildViewController.swift
+++ SoundsLyric/SongEditChildViewController.swift
@@ -36,16 +36,11 @@ class SongEditChildViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - UITextViewDelegate
+extension SongEditChildViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
     }
-    */
-
 }
